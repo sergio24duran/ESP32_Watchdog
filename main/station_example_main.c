5a5865f1732e8dfc07874b1a1fbaabab70de6f6a
sergio24duran <sergioduranmartin24@gmail.com>
2024-12-16 21:57:12 +0100

    pre add new main

--- main/station_example_main.c
+++ main/station_example_main.c
@@ -16,6 +16,8 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 
+#include <esp_http_client.h>
+
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
@@ -24,9 +26,9 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
-#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
-#define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
+#define EXAMPLE_ESP_WIFI_SSID      "R2DLAN"
+#define EXAMPLE_ESP_WIFI_PASS      "crimpadora24@"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  3
 
 #if CONFIG_ESP_WPA3_SAE_PWE_HUNT_AND_PECK
 #define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HUNT_AND_PECK
